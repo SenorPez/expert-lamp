be03f4ac37e6a3896a81a931c036ac2251d42cea
SenorPez <senorpez@senorpez.com>
2021-11-09 16:25:43 -0600

    Update Prices class to get all from API.

--- guildwars2api/src/guildwars2api/commerce.py
+++ guildwars2api/src/guildwars2api/commerce.py
@@ -9,18 +9,34 @@ import requests
 class Prices:
     """Represents aggregated buy and sell listing information in an array of Price objects
     API Endpoint: /v2/commerce/prices/<item_ids>
-
-    Arguments:
-        item_ids - Array of item IDs.
     """
     scheme = "https"
     host = "api.guildwars2.com"
     endpoint = "/v2/commerce/prices"
 
-    def __init__(self, item_ids):
+    def __init__(self, item_ids=None):
         request_url = urlunsplit((self.scheme, self.host, self.endpoint, '', ''))
-        params = {"ids": ",".join(map(str, item_ids))}
-        req = requests.get(request_url, params=params)
-        req.raise_for_status()
 
-        self.values = req.json()
+        if item_ids is None:
+            req = requests.get(request_url)
+            req.raise_for_status()
+            item_ids = req.json()
+            chunked_item_ids = self.chunk_array(item_ids, 100)
+
+            self.values = []
+            for chunk in chunked_item_ids:
+                self.values.extend(self.get_api_data(request_url, chunk))
+        else:
+            self.values = self.get_api_data(request_url, item_ids)
+
+    @staticmethod
+    def chunk_array(array, chunk_length):
+        for i in range(0, len(array), chunk_length):
+            yield array[i:i + chunk_length]
+
+    @staticmethod
+    def get_api_data(url, item_ids):
+        params = {"ids": ",".join(map(str, item_ids))}
+        req = requests.get(url, params=params)
+        req.raise_for_status()
+        return req.json()
--- guildwars2api/test/guildwars2api/test_commerce.py
+++ guildwars2api/test/guildwars2api/test_commerce.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest import mock
-from unittest.mock import sentinel
+from unittest.mock import sentinel, call
 
 from src.guildwars2api.commerce import Prices
 
@@ -53,6 +53,24 @@ class TestPrices(unittest.TestCase):
         mock_get.assert_called_with(url, params=params)
 
     @mock.patch('src.guildwars2api.commerce.requests.get')
+    def test_init_all(self, mock_get):
+        json_ids = [id(sentinel.item_id_1), id(sentinel.item_id_2)]
+        json_data_1 = mock_commerce_prices_item(id(sentinel.item_id_1), False, id(sentinel.buys_quantity),
+                                              id(sentinel.buys_unit_price), id(sentinel.sells_quantity),
+                                              id(sentinel.sells_unit_price))
+        json_data_2 = mock_commerce_prices_item(id(sentinel.item_id_2), False, id(sentinel.buys_quantity),
+                                              id(sentinel.buys_unit_price), id(sentinel.sells_quantity),
+                                              id(sentinel.sells_unit_price))
+        mock_get.side_effect = [mocked_requests_get(json_data=json_ids), mocked_requests_get(json_data=[json_data_1, json_data_2])]
+        url = "https://api.guildwars2.com/v2/commerce/prices"
+        params = {"ids": ",".join(map(str, [id(sentinel.item_id_1), id(sentinel.item_id_2)]))}
+
+        instance = Prices()
+        expected_value = Prices
+        self.assertIsInstance(instance, expected_value)
+        mock_get.assert_has_calls([call(url), call(url, params=params)])
+
+    @mock.patch('src.guildwars2api.commerce.requests.get')
     def test_values(self, mock_get):
         json_data = mock_commerce_prices_item(id(sentinel.item_id), False, id(sentinel.buys_quantity),
                                               id(sentinel.buys_unit_price), id(sentinel.sells_quantity),
@@ -75,6 +93,34 @@ class TestPrices(unittest.TestCase):
         self.assertEqual([x['sells']['quantity'] for x in instance.values], expected_sells_quantity)
         self.assertEqual([x['sells']['unit_price'] for x in instance.values], expected_sells_unit_price)
         mock_get.assert_called_with(url, params=params)
+
+    @mock.patch('src.guildwars2api.commerce.requests.get')
+    def test_values_all(self, mock_get):
+        json_ids = [id(sentinel.item_id_1), id(sentinel.item_id_2)]
+        json_data_1 = mock_commerce_prices_item(id(sentinel.item_id_1), False, id(sentinel.buys_quantity),
+                                                id(sentinel.buys_unit_price), id(sentinel.sells_quantity),
+                                                id(sentinel.sells_unit_price))
+        json_data_2 = mock_commerce_prices_item(id(sentinel.item_id_2), False, id(sentinel.buys_quantity),
+                                                id(sentinel.buys_unit_price), id(sentinel.sells_quantity),
+                                                id(sentinel.sells_unit_price))
+        mock_get.side_effect = [mocked_requests_get(json_data=json_ids), mocked_requests_get(json_data=[json_data_1, json_data_2])]
+        url = "https://api.guildwars2.com/v2/commerce/prices"
+        params = {"ids": ",".join(map(str, [id(sentinel.item_id_1), id(sentinel.item_id_2)]))}
+
+        instance = Prices()
+        expected_ids = [id(sentinel.item_id_1), id(sentinel.item_id_2)]
+        expected_whitelists = [False, False]
+        expected_buys_quantitys = [id(sentinel.buys_quantity), id(sentinel.buys_quantity)]
+        expected_buys_unit_prices = [id(sentinel.buys_unit_price), id(sentinel.buys_unit_price)]
+        expected_sells_quantitys = [id(sentinel.sells_quantity), id(sentinel.sells_quantity)]
+        expected_sells_unit_prices = [id(sentinel.sells_unit_price), id(sentinel.sells_unit_price)]
+        self.assertEqual([x['id'] for x in instance.values], expected_ids)
+        self.assertEqual([x['whitelisted'] for x in instance.values], expected_whitelists)
+        self.assertEqual([x['buys']['quantity'] for x in instance.values], expected_buys_quantitys)
+        self.assertEqual([x['buys']['unit_price'] for x in instance.values], expected_buys_unit_prices)
+        self.assertEqual([x['sells']['quantity'] for x in instance.values], expected_sells_quantitys)
+        self.assertEqual([x['sells']['unit_price'] for x in instance.values], expected_sells_unit_prices)
+        mock_get.assert_has_calls([call(url), call(url, params=params)])
 
 
 if __name__ == '__main__':
