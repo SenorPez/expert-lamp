69fd33c17ba8cbd11af896d6b0a86b212a952f15
SenorPez <senorpez@senorpez.com>
2021-11-22 16:19:44 -0600

    Created Java project and Item class.

---
+++ guildwars2/src/main/java/com/senorpez/guildwars2/api/Item.java
@@ -0,0 +1,117 @@
+package com.senorpez.guildwars2.api;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Item {
+    private final static String protocol = "https";
+    private final static String host = "api.guildwars2.com";
+    private final static String resource = "/v2/items";
+
+    private final int id;
+    private final String name;
+
+    public Item(ObjectNode itemJson) {
+        this.id = itemJson.get("id").asInt();
+        this.name = itemJson.get("name").asText();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Item getSingle(int itemId) throws IOException {
+        String params = "?ids=" + itemId;
+        Stream<Item> items = Item.callAPI(params);
+        return items.findFirst().orElseThrow();
+    }
+
+    public static Stream<Item> getMultiple(Stream<Integer> itemIds) throws IOException {
+        Stream<Stream<Integer>> chunks = chunkIds(itemIds);
+        Stream<Stream<Item>> itemChunks = chunks.map(chunk -> {
+            String params = "?ids=" + chunk.map(Object::toString).collect(Collectors.joining(","));
+            try {
+                return Item.callAPI(params);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        });
+        return itemChunks.reduce(Stream::concat).orElse(Stream.of());
+    }
+
+    public static Stream<Item> getAll() throws IOException {
+        URL url = new URL(Item.protocol, Item.host, Item.resource);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        if (con.getResponseCode() == 200) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            ArrayNode json = (ArrayNode) objectMapper.readTree(con.getInputStream());
+            Stream.Builder<Integer> integerBuilder = Stream.builder();
+            json.forEach(node -> integerBuilder.accept(node.asInt()));
+
+            Stream<Stream<Integer>> chunks = chunkIds(integerBuilder.build());
+            Stream<Stream<Item>> itemChunks = chunks.map(chunk -> {
+                try {
+                    return getMultiple(chunk);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            });
+            return itemChunks.reduce(Stream::concat).orElse(Stream.of());
+        } else {
+            throw new IOException(String.valueOf(con.getResponseCode()));
+        }
+    }
+
+    private static Stream<Item> callAPI(String params) throws IOException {
+        URL url = new URL(Item.protocol, Item.host, Item.resource + params);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        if (con.getResponseCode() == 200) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            ArrayNode json = (ArrayNode) objectMapper.readTree(con.getInputStream());
+            Stream.Builder<Item> itemBuilder = Stream.builder();
+            json.forEach(node -> itemBuilder.accept(new Item((ObjectNode) node)));
+            return itemBuilder.build();
+        } else {
+            throw new IOException(String.valueOf(con.getResponseCode()));
+        }
+    }
+
+    private static Stream<Stream<Integer>> chunkIds(Stream<Integer> item_ids) {
+        LinkedList<Integer> list = item_ids.collect(Collectors.toCollection(LinkedList::new));
+        Stream.Builder<Stream<Integer>> returnValue = Stream.builder();
+
+        int baseLength = (Item.protocol + "://" + Item.host + Item.resource + "?ids=").length();
+
+        while (list.peek() != null) {
+            Stream.Builder<Integer> builder = Stream.builder();
+            int paramsLength = 0;
+
+            while (list.peek() != null && baseLength + paramsLength + list.peek().toString().length() + 1 < 255) {
+                builder.accept(list.peek());
+                paramsLength += list.remove().toString().length() + 1;
+            }
+            returnValue.accept(builder.build());
+        }
+        return returnValue.build();
+    }
+}
